2f2700160909aed14a37d2d5a512f429cac4c42b
abdou kowiyou BACHABI <abdoubachabikowiyou@gmail.com>
2024-01-12 17:11:29 +0100

    add chatlive

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,55 +1,68 @@
     <footer>
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API = Tawk_API || {},
+                Tawk_LoadStart = new Date();
+            (function() {
+                var s1 = document.createElement("script"),
+                    s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/65a15a118d261e1b5f5278ec/1hjv4fm6v';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
+            })();
+        </script>
+        <!--End of Tawk.to Script-->
         <div class="container">
             <div class="row piedpage">
                 <div class="col">
-                  <h5 >La Brocante</h5>
-                  <ul class="list-unstyled">
-                    <li>
-                      <a href="#!">A propos</a>
-                    </li>
-                    <li>
-                      <a href="#!">Actualités</a>
-                    </li>
-                  </ul>
+                    <h5>La Brocante</h5>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!">A propos</a>
+                        </li>
+                        <li>
+                            <a href="#!">Actualités</a>
+                        </li>
+                    </ul>
                 </div>
 
                 <div class="col">
-                  <h5>Découvrir</h5>
-                  <ul class="list-unstyled">
-                    <li>
-                      <a href="#!">Comment ca marche</a>
-                    </li>
-                    <li>
-                      <a href="#!">Nouveauté</a>
-                    </li>
-                  </ul>
+                    <h5>Découvrir</h5>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!">Comment ca marche</a>
+                        </li>
+                        <li>
+                            <a href="#!">Nouveauté</a>
+                        </li>
+                    </ul>
                 </div>
 
                 <div class="col">
-                  <h5 >Aides</h5>
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#!">Centre d'aide</a>
-                    </li>
-                    <li>
-                      <a href="#!">Sécurité</a>
-                    </li>
-                  </ul>
+                    <h5>Aides</h5>
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a href="#!">Centre d'aide</a>
+                        </li>
+                        <li>
+                            <a href="#!">Sécurité</a>
+                        </li>
+                    </ul>
                 </div>
 
             </div>
             <div class="linefooter">
                 <div class="line"></div>
-                    <div class="social-icons">
-                        <a href=""><i class="bi bi-facebook"></i></a>
-                        <a href=""><i class="bi bi-linkedin"></i></a>
-                        <a href=""><i class="bi bi-instagram"></i></a>
-                        <a href=""><i class="bi bi-twitter"></i></a>
-                    </div>
+                <div class="social-icons">
+                    <a href=""><i class="bi bi-facebook"></i></a>
+                    <a href=""><i class="bi bi-linkedin"></i></a>
+                    <a href=""><i class="bi bi-instagram"></i></a>
+                    <a href=""><i class="bi bi-twitter"></i></a>
+                </div>
                 <div class="line"></div>
             </div>
         </div>
 
     </footer>
-
-
